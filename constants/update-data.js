a823977b014c067d3a4448f71dcc60676709cc63
GithubTutorialCourse <113823532+GithubTutorialCourse@users.noreply.github.com>
2022-09-18 14:46:54 +0700

    Add files via upload

---
+++ constants/update-data.js
@@ -0,0 +1,20 @@
+const UPDATE_DATA = [
+  {
+    imgSrc: "./images/profile-2.png",
+    profileName: "Mike Tyson",
+    message: "received his order of Night lion tech GPS drone.",
+    updatedTime: "2 Minutes Ago",
+  },
+  {
+    imgSrc: "./images/profile-3.png",
+    profileName: "Diana Ayi",
+    message: "declined her order of 2 DJI Air 2S.",
+    updatedTime: "5 Minutes Ago",
+  },
+  {
+    imgSrc: "./images/profile-4.png",
+    profileName: "Mandy Roy",
+    message: "received his order of LARVENDER KF102 Drone.",
+    updatedTime: "6 Minutes Ago",
+  },
+];
